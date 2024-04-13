24997a64d328b39edbbc16aebff32d0b659a63cf
Shivansh Gupta <shivanshgupta0987+github@gmail.com>
2024-04-13 11:04:27 +0530

    Added all files

---
+++ More_pages/form-handler.php
@@ -0,0 +1,29 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['Message'];
+
+$email_form = 'Quantum coders';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name : $name.\n".
+                "User Email : $visitor_email.\n".
+                "Subject: $subject.\n".
+                "User Message: $message.\n";
+
+
+$to = 'shivanshgupta0987@gmail.com';
+
+$headers = "Form: $email_form \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+
+
+?>
